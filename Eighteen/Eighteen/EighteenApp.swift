aa4ab8dfe769d216086215d849ca6d57c8e0a4bb
DongHo Choe <hamfan524@naver.com>
2024-03-11 16:55:23 +0900

    ✨ Add: 프로젝트 파일 추가
    Co-Authored-By: 황민채 <103357078+Hminchae@users.noreply.github.com>
    Co-Authored-By: Hsungjin <120264964+Hsungjin@users.noreply.github.com>
    Co-Authored-By: Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
    Co-Authored-By: Phangg <109324421+Phangg@users.noreply.github.com>
    Co-Authored-By: Timmy <80156515+bdrsky2010@users.noreply.github.com>
    Co-Authored-By: kim.c_cha.nam <144766297+kmh5038@users.noreply.github.com>

---
+++ Eighteen/Eighteen/EighteenApp.swift
@@ -0,0 +1,17 @@
+//
+//  EighteenApp.swift
+//  Eighteen
+//
+//  Created by 최동호 on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EighteenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
