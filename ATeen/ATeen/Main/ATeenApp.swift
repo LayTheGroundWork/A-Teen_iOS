ccdee7695cec28e5923de9385c40382d320cb85c
NohJooYoung <nohjoo97@naver.com>
2024-04-15 18:37:12 +0900

    ✨ Add: 라이브러리 추가
    - 카카오 로그인
    
    Co-Authored-By: Lee ChangJun <109324421+Phangg@users.noreply.github.com>
    Co-Authored-By: DongHo Choe <hamfan524@naver.com>
    Co-Authored-By: kim.c_cha.nam <144766297+kmh5038@users.noreply.github.com>

--- ATeen/ATeen/Main/ATeenApp.swift
+++ ATeen/ATeen/Main/ATeenApp.swift
@@ -6,6 +6,8 @@
 //
 
 import ComposableArchitecture
+import KakaoSDKAuth
+import KakaoSDKCommon
 
 import SwiftUI
 
@@ -16,12 +18,24 @@ struct ATeenApp: App {
             ._printChanges()
     }
     
+    init() {
+        if let kakaoURL = Bundle.main.kakaoURL {
+            KakaoSDK.initSDK(appKey: kakaoURL)
+        } else {
+            print("kakaoAppKey를 찾을 수 없습니다.")
+        }
+    }
     
     var body: some Scene {
         WindowGroup {
             MainTabView(
                 store: ATeenApp.store
             )
+            .onOpenURL { url in // 뷰가 속한 Window에 대한 URL을 받았을 때 호출할 Handler를 등록하는 함수
+                if AuthApi.isKakaoTalkLoginUrl(url) {
+                    _ = AuthController.handleOpenUrl(url: url)
+                }
+            }
         }
     }
 }
