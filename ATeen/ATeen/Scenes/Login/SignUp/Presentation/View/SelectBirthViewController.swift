10101a4085d18241546b5564c9fc6688707c3e6e
NohJooYoung <nohjoo97@naver.com>
2024-06-03 23:43:20 +0900

    💄 Design: Blur 제거
    Co-Authored-By: Lee ChangJun <109324421+Phangg@users.noreply.github.com>
    Co-Authored-By: DongHo Choe <hamfan524@naver.com>

--- ATeen/ATeen/Scenes/Login/SignUp/Presentation/View/SelectBirthViewController.swift
+++ ATeen/ATeen/Scenes/Login/SignUp/Presentation/View/SelectBirthViewController.swift
@@ -128,7 +128,7 @@ final class SelectBirthViewController: UIViewController {
     
     private func configUserInterface() {
         //메인 뷰
-        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+        view.backgroundColor = .black.withAlphaComponent(0.5)
         
         view.addSubview(alertView)
         
