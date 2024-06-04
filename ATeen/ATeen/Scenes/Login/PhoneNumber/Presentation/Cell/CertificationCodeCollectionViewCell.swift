5a60477c3cc397fc1bb669fab5ca278566ab940e
kangciu <a2849535@gmail.com>
2024-06-04 12:51:30 +0900

    🩹 Refactor: 인증코드 뷰 버그 수정
    텍스트필드 이전으로 돌아가게 되면 한 텍스트필드에 여러 값이 들어가는 버그 수정
    
    숫자만 들어갈 수 있도록 텍스트필드 입력 값이 숫자인지 검증하는 로직 추가
    
    feat. 여전히 텍스트필드 입력값이 없을 때 뒤로가지 못하는 현상은 수정 못했음..

--- ATeen/ATeen/Scenes/Login/PhoneNumber/Presentation/Cell/CertificationCodeCollectionViewCell.swift
+++ ATeen/ATeen/Scenes/Login/PhoneNumber/Presentation/Cell/CertificationCodeCollectionViewCell.swift
@@ -23,16 +23,14 @@ final class CertificationCodeCollectionViewCell: UICollectionViewCell {
     private lazy var inputCodeLabel: UILabel = {
         let label = UILabel()
         label.text = AppLocalized.inputCodeText
-        label.font = UIFont.customFont(forTextStyle: .title3,
-                                       weight: .bold)
+        label.font = UIFont.customFont(forTextStyle: .title3, weight: .bold)
         label.textColor = .black
         return label
     }()
     
     private lazy var nextButton: UIButton = {
         let button = UIButton()
-        button.titleLabel?.font = UIFont.customFont(forTextStyle: .callout,
-                                                    weight: .regular)
+        button.titleLabel?.font = UIFont.customFont(forTextStyle: .callout, weight: .regular)
         button.setTitle(AppLocalized.nextButton, for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.backgroundColor = .black
@@ -45,8 +43,7 @@ final class CertificationCodeCollectionViewCell: UICollectionViewCell {
     private lazy var resendLabel: UILabel = {
         let label = UILabel()
         label.text = AppLocalized.resendText
-        label.font = UIFont.customFont(forTextStyle: .footnote,
-                                       weight: .regular)
+        label.font = UIFont.customFont(forTextStyle: .footnote, weight: .regular)
         label.textColor = .gray02
         return label
     }()
@@ -54,8 +51,7 @@ final class CertificationCodeCollectionViewCell: UICollectionViewCell {
     private lazy var resendButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle(AppLocalized.resendButtonText, for: .normal)
-        button.titleLabel?.font = UIFont.customFont(forTextStyle: .footnote,
-                                                    weight: .regular)
+        button.titleLabel?.font = UIFont.customFont(forTextStyle: .footnote, weight: .regular)
         button.setTitleColor(.main, for: .normal)
         button.addTarget(self, action: #selector(didSelectResendButton(_:)), for: .touchUpInside)
         return button
@@ -79,7 +75,6 @@ final class CertificationCodeCollectionViewCell: UICollectionViewCell {
         configLayout()
         setupActions()
     }
-    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -216,28 +211,35 @@ extension CertificationCodeCollectionViewCell: UITextFieldDelegate {
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         guard let text = textField.text, let textRange = Range(range, in: text) else { return false }
+        
+        // 텍스트가 숫자인지 확인
+        guard string.isEmpty || (string.rangeOfCharacter(from: CharacterSet.decimalDigits) != nil) else {
+            return false
+        }
 
         let updatedText = text.replacingCharacters(in: textRange, with: string)
-        
-        // 텍스트 필드에 텍스트 설정
-        textField.text = updatedText
         
         /// 입력이 비어 있는 경우 이전 텍스트 필드로 이동
         /// **현재 입력이 비어있어도 이전 텍스트 필드로 이동을 못하는 버그가 있음**
         if string.isEmpty {
-            if updatedText.isEmpty, textField.tag > 1 {
+            textField.text = ""
+            if textField.tag > 1 {
                 let previousTextField = textFields[textField.tag - 2]
                 previousTextField.becomeFirstResponder()
             }
         } else {
+            textField.text = String(string.prefix(1))
             // 입력 시 다음 필드로 이동
-            if updatedText.count == 1 {
-                if textField.tag < textFields.count {
-                    let nextTextField = textFields[textField.tag]
-                    nextTextField.becomeFirstResponder()
+            if textField.tag < textFields.count {
+                let nextTextField = textFields[textField.tag]
+                if nextTextField.text?.isEmpty == false {
+                    nextTextField.text = "" // 이미 값이 있을 경우 지우기
+                    nextTextField.becomeFirstResponder() // 그리고 포커스 이동
                 } else {
-                    textField.resignFirstResponder()
+                    nextTextField.becomeFirstResponder() // 빈 경우 바로 포커스 이동
                 }
+            } else {
+                textField.resignFirstResponder()
             }
         }
         
