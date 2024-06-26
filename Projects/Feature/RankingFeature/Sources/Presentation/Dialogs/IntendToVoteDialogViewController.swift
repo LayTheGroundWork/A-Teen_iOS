447fb22e64a743c94b7c7e31dec7557a3a5287b2
Phangg <leeckdwns9305@gmail.com>
2024-06-26 18:55:30 +0900

    🏫 Feat: RankingFeature - Dialog 화면 2개 구성 #20
    IntendToVoteDialogViewController
    - 투표 참여 확인 다이얼로그
    
    QuitTournamentDialogViewController
    - 투표 종료 확인 다이얼로그

---
+++ Projects/Feature/RankingFeature/Sources/Presentation/Dialogs/IntendToVoteDialogViewController.swift
@@ -0,0 +1,76 @@
+//
+//  IntendToVoteDialogViewController.swift
+//  RankingFeature
+//
+//  Created by phang on 6/26/24.
+//  Copyright © 2024 ATeen. All rights reserved.
+//
+
+import DesignSystem
+import UIKit
+
+final class IntendToVoteDialogViewController: CustomTwoButtonDialogViewController {
+    let sector: String
+    
+    // MARK: - Life Cycle
+    public init(
+        sector: String,
+        dialogImage: UIImage? = nil,
+//        dialogImage: UIImage? = UIImage(named: "hrmi"),
+        dialogTitle: String? = nil,
+        titleColor: UIColor = .black,
+        titleNumberOfLine: Int = 1,
+        titleFont: UIFont = .customFont(forTextStyle: .callout, weight: .bold),
+        dialogMessage: String = "투표 설명이 들어갈 위치",
+        messageColor: UIColor = DesignSystemAsset.gray02.color,
+        messageNumberOfLine: Int = 1,
+        messageFont: UIFont = .customFont(forTextStyle: .footnote, weight: .regular),
+        leftButtonText: String = "다음에 할게요",
+        leftButtonColor: UIColor = DesignSystemAsset.gray01.color,
+        rightButtonText: String = "네, 참여할래요!",
+        rightButtonColor: UIColor = DesignSystemAsset.mainColor.color
+    ) {
+        let dialogTitle = dialogTitle ?? "\(sector) 투표에 참여하시겠어요?"
+        
+        self.sector = sector
+        super.init(
+            dialogImage: dialogImage,
+            dialogTitle: dialogTitle,
+            titleNumberOfLine: titleNumberOfLine,
+            dialogMessage: dialogMessage,
+            messageNumberOfLine: messageNumberOfLine,
+            leftButtonText: leftButtonText,
+            leftButtonColor: leftButtonColor,
+            rightButtonText: rightButtonText,
+            rightButtonColor: rightButtonColor
+        )
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Actions
+    public override func clickLeftButton(_ sender: UIButton) {
+        super.clickLeftButton(sender)
+        // TODO: - 다이얼로그 닫기
+//        self.dismiss(animated: false)
+    }
+    
+    public override func clickRightButton(_ sender: UIButton) {
+        super.clickRightButton(sender)
+        // TODO: - 토너먼트 진행 화면 이동
+//        let tournamentVC = TournamentViewController(sector: "뷰티")
+//        
+//        if let presentingVC = self.presentingViewController {
+//            self.dismiss(animated: false) {
+//                if let navigationController = presentingVC as? UINavigationController {
+//                    navigationController.pushViewController(tournamentVC, animated: true)
+//                } else {
+//                    let navController = UINavigationController(rootViewController: tournamentVC)
+//                    self.presentingViewController?.present(navController, animated: true)
+//                }
+//            }
+//        }
+    }
+}
---
+++ Projects/Feature/RankingFeature/Sources/Presentation/Dialogs/QuitTournamentDialogViewController.swift
@@ -0,0 +1,66 @@
+//
+//  QuitTournamentDialogViewController.swift
+//  RankingFeature
+//
+//  Created by phang on 6/26/24.
+//  Copyright © 2024 ATeen. All rights reserved.
+//
+
+import DesignSystem
+import UIKit
+
+protocol QuitTournamentDialogViewControllerDelegate: AnyObject {
+    func endTournament()
+}
+
+final class QuitTournamentDialogViewController: CustomTwoButtonDialogViewController {
+    weak var delegate: QuitTournamentDialogViewControllerDelegate?
+    
+    // MARK: - Life Cycle
+    public init(
+        delegate: QuitTournamentDialogViewControllerDelegate,
+        dialogImage: UIImage? = nil,
+        dialogTitle: String? = "정말로 그만 두시겠어요?",
+        titleColor: UIColor = .black,
+        titleNumberOfLine: Int = 1,
+        titleFont: UIFont = .customFont(forTextStyle: .callout, weight: .bold),
+        dialogMessage: String = "여기서 그만 두면\n처음부터 투표를 다시 해야해요!",
+        messageColor: UIColor = DesignSystemAsset.gray02.color,
+        messageNumberOfLine: Int = 2,
+        messageFont: UIFont = .customFont(forTextStyle: .footnote, weight: .regular),
+        leftButtonText: String = "그만 둘래요",
+        leftButtonColor: UIColor = DesignSystemAsset.gray01.color,
+        rightButtonText: String = "마저 할게요!",
+        rightButtonColor: UIColor = DesignSystemAsset.mainColor.color
+    ) {
+        self.delegate = delegate
+        super.init(
+            dialogTitle: dialogTitle,
+            titleNumberOfLine: titleNumberOfLine,
+            dialogMessage: dialogMessage,
+            messageNumberOfLine: messageNumberOfLine,
+            leftButtonText: leftButtonText,
+            leftButtonColor: leftButtonColor,
+            rightButtonText: rightButtonText,
+            rightButtonColor: rightButtonColor
+        )
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Actions
+    public override func clickLeftButton(_ sender: UIButton) {
+        super.clickLeftButton(sender)
+        // TODO: - 다이얼로그 닫기 & 토너먼트 닫기
+//        self.dismiss(animated: false)
+//        delegate?.endTournament()
+    }
+    
+    public override func clickRightButton(_ sender: UIButton) {
+        super.clickRightButton(sender)
+        // TODO: - 다이얼로그 닫기
+//        self.dismiss(animated: false)
+    }
+}
