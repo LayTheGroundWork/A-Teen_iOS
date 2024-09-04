64faa6d08e05a93355a2f6bd2617cc7ef0ff6365
DongHo Choe <hamfan524@naver.com>
2024-09-04 17:37:43 +0900

    ✨ Add: SocketIO 추가

--- Projects/FeatureDependency/Project.swift
+++ Projects/FeatureDependency/Project.swift
@@ -14,6 +14,7 @@ let project = Project.makeProject(
     dependencies: [
         .designSystem,
         .common,
-        .domain
+        .domain,
+        .external(name: "SocketIO")
     ]
 )
--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -8,7 +8,10 @@ import PackageDescription
         // Customize the product types for specific package product
         // Default is .staticFramework
         // productTypes: ["Alamofire": .framework,] 
-        productTypes: ["SnapKit": .framework]
+        productTypes: [
+            "SnapKit": .framework,
+            "SocketIO": .framework
+        ]
     )
 #endif
 
@@ -18,6 +21,7 @@ let package = Package(
         // Add your own dependencies here:
         // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
         // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
-        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1"))
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
+        .package(url: "https://github.com/socketio/socket.io-client-swift", .upToNextMajor(from: "15.0.0"))
     ]
 )
